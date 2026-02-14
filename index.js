f18d1e97a4f73710462cb9af3035fea037b51b1a
SCRIPTHUB <20a83g8m@gmail.com>
2026-02-13 20:26:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
+const { Client, GatewayIntentBits, PermissionsBitField } = require('discord.js');
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ticket') {
+    const channel = await interaction.guild.channels.create({
+      name: `ticket-${interaction.user.username}`,
+      type: 0,
+      permissionOverwrites: [
+        {
+          id: interaction.guild.id,
+          deny: [PermissionsBitField.Flags.ViewChannel]
+        },
+        {
+          id: interaction.user.id,
+          allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages]
+        }
+      ]
+    });
+
+    await interaction.reply({ content: `Ticket created: ${channel}`, ephemeral: true });
+  }
+});
+
+client.login(process.env.TOKEN);
